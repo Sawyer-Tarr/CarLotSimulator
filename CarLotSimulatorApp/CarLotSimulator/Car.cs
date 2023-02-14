55c02636befeb7c62d5d9c52e390566b0ea7e3b2
sawyer tarr <sawyertarr@outlook.com>
2023-02-14 11:34:42 -0600

    AddedCars

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car() 
+        {
+            
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDrivable = isDrivable;
+        }
+
+
+        public void MakeEngineNoise()
+        { 
+            Console.WriteLine($"The {Model} needs some work under the hood. Its makeing a {EngineNoise} noise."); 
+        }
+        public void MakeHonkNoise() 
+        {
+            Console.WriteLine($"The {Make}'s horn might be broken. It sounds like {HonkNoise}."); 
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -22,12 +22,60 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+            CarLot sawyerCarLot = new CarLot();
+
+            
+
+            //Object Initializer
+            Car currentCar = new Car()
+            {
+                Make = "Chevrolet",
+                Year = 2015,
+                Model = "Silverado",
+                IsDrivable = true,
+                EngineNoise = "Vroom",
+                HonkNoise = "Honk",
+            };
+            currentCar.MakeEngineNoise();
+            currentCar.MakeHonkNoise();
+
+            sawyerCarLot.ListOfCars.Add(currentCar);
+
+            //Dot Notation
+            Car nextCar= new Car();
+            nextCar.Make = "Chevrolet";
+            nextCar.Year = 1969;
+            nextCar.Model = "Camero";
+            nextCar.IsDrivable = true;
+            nextCar.EngineNoise = "VlomVlom";
+            nextCar.HonkNoise = "BamphBamph";
+
+            nextCar.MakeEngineNoise();
+            nextCar.MakeHonkNoise();
+
+            sawyerCarLot.ListOfCars.Add(nextCar);
+
+            //Custom
+            var oldCar = new Car(2003, "Nissan", "Pathfinder", "romrom", "beep beep", true);
+
+            oldCar.MakeEngineNoise();
+            oldCar.MakeHonkNoise();
+
+            sawyerCarLot.ListOfCars.Add(oldCar);
+
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            foreach (var currentcar in sawyerCarLot.ListOfCars)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Year: {currentcar.Year}. Make: {currentcar.Make}. Model: {currentcar.Model}");
+                Console.WriteLine();
+            }
         }
     }
 }
